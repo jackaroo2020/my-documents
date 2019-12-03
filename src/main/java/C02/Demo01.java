7910d420b4e3e038c3f9ee5685fb28f0baec8742
LT <jackaroo1024@163.com>
2019-12-03 13:05:19 +0800

    Java面试题集

---
+++ src/main/java/C02/Demo01.java
@@ -0,0 +1,16 @@
+package C02;
+
+/**
+ * @Description: javaÀà×÷ÓÃÃèÊö
+ * @Author: LT
+ * @CreateDate: 2019/12/3 11:19
+ * @Version: 1.0.0
+ */
+public class Demo01 {
+
+    public static void main(String[] args) {
+        //float f = 3.4f;
+        float f = (float)3.4;
+        System.out.println(f);
+    }
+}
